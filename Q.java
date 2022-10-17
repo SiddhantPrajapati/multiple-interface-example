537073a84b9388a7a89badb2baf2ff1f580f3b78
SiddhantPrajapati <110342624+SiddhantPrajapati@users.noreply.github.com>
2022-10-17 23:58:49 +0530

    Add files via upload

---
+++ Q.java
@@ -0,0 +1,14 @@
+class Q implements P12 {
+	public void disP() {
+	System.out.println("value of P : "+ap);
+}
+	public void disP1() {
+	System.out.println("value of P1 : "+ap1);
+	}
+	public void disP2() {
+	System.out.println("value of P2 : "+ap2);
+	}
+	public void disP12() {
+	System.out.println("value of P12 : "+ap12);
+	}
+}
---
+++ TestQ.java
@@ -0,0 +1,9 @@
+public class TestQ {
+	public static void main(String[] args) {
+	Q q = new Q();
+	q.disP();
+	q.disP1();
+	q.disP2();
+	q.disP12();
+	}
+}
